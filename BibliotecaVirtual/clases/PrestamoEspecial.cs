37b2af3ae7be24702005730d0c140279a1a79b5a
JonathanSepulveda <jonathansc25@gmail.com>
2016-06-18 16:13:32 -0400

    Clases Préstamo Normal y Préstamo Especial
    Creación de clases Préstamo Normal y Préstamo Especial con sus
    respectivos constructores (vacíos).

---
+++ BibliotecaVirtual/clases/PrestamoEspecial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.clases
+{
+    class PrestamoEspecial
+    {
+
+        /**
+         * 
+         *      
+         */
+       
+       public PrestamoEspecial(int Id,String fechaPrestamo, string fechaDevolucion, string fechaRealDevolucion, string estadoPrestamo)
+        {
+
+        }
+
+
+       public static Boolean isAtrasosVigentes() {
+
+           return true;
+       
+       }
+       public static void setDB()
+       {
+
+
+       }
+
+
+    }
+}
---
+++ BibliotecaVirtual/clases/PrestamoNormal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.clases
+{
+    class PrestamoNormal
+    {
+       
+
+        public PrestamoNormal(int Id,String fechaPrestamo, string fechaDevolucion, string fechaRealDevolucion, string estadoPrestamo)
+        {
+
+        }
+
+        public static void setDB()
+        {
+
+        }
+
+        public static void setDevolucion()
+        {
+
+        }
+
+    }
+}
