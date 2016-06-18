5c96a02c49de2713b27736655667f0b882874ce3
goodmanc <cristian.a.guzman.merino@gmail.com>
2016-06-18 16:12:37 -0400

    Clases adicionales de modulos en paralelo para simulación

---
+++ BibliotecaVirtual/clases/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.clases
+{
+    class Alumno
+    {
+
+        public Alumno(int añoIngreso, String FechaNacimiento)
+        {
+
+        }
+
+    }
+}
---
+++ BibliotecaVirtual/clases/RegistroMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.clases
+{
+    class RegistroMaterial
+    {
+        public RegistroMaterial(int Registro, String FechaRegistro, String Tipo)
+        {
+
+        }
+
+    }
+}
