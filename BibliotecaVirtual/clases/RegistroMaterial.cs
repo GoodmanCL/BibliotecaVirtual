e33e28c50e4d766f327209ac692d643b0900e257
goodmanc <cristian.a.guzman.merino@gmail.com>
2016-06-18 16:13:39 -0400

    Clase adicional RegistroMaterial

--- BibliotecaVirtual/clases/RegistroMaterial.cs
+++ BibliotecaVirtual/clases/RegistroMaterial.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace BibliotecaVirtual.clases
 {
+    
+
     class RegistroMaterial
     {
         public RegistroMaterial(int Registro, String FechaRegistro, String Tipo)
