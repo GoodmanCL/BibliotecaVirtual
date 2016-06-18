304d2a391f124c4453934e47feae9c34ccf77bf9
xkako <heberinostrozafuentes@gmail.com>
2016-06-18 16:06:42 -0400

    Creación de Clases AtrasNormal y AtrasoEspecial

---
+++ BibliotecaVirtual/clases/AtrasoEspecial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.clases
+{
+    class AtrasoEspecial
+    {
+
+        public AtrasoEspecial()
+        {
+
+        }
+
+        public static void setBD(){
+
+        }
+
+        public static void cumplioSancion(String alumno) {
+
+        }
+    }
+}
