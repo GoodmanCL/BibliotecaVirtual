424cedaccb571dbc2db02ccc631c107eb804aaa1
goodmanc <cristian.a.guzman.merino@gmail.com>
2016-06-20 21:34:34 -0400

    Modificación de clase Prestamo ligada a Bibliotecario y Usuario
    Se agregaron las propiedades
    + idBibliotecario: que almacena el id del Blibliotecario responsable de
    registrar el Préstamo
    + idUsuario: que almacena el id del Usuario al que se le otorga el
    Préstamo

--- BibliotecaVirtual/clases/Prestamo.cs
+++ BibliotecaVirtual/clases/Prestamo.cs
@@ -23,6 +23,21 @@ namespace BibliotecaVirtual.clases
             set { id = value; }
         }
 
+        private int idBibliotecario;
+
+        public int IdBibliotecario
+        {
+            get { return idBibliotecario; }
+            set { idBibliotecario = value; }
+        }
+        private int idUsuario;
+
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
         private string fechaPrestamo;     
 
         public string FechaPrestamo
@@ -58,10 +73,11 @@ namespace BibliotecaVirtual.clases
         }
 
 
-        public Prestamo(int id, string fechaPrestamo, string fechaDevolucion, string fechaRealDevolucion, string estadoPrestamo)  //constructor de clase prestamo
+        public Prestamo(int id, int idBibliotecario, int idUsuario, string fechaPrestamo, string fechaDevolucion, string fechaRealDevolucion, string estadoPrestamo)  //constructor de clase prestamo
         {
             this.Id = id;
-            this.FechaPrestamo = fechaPrestamo;
+            this.IdBibliotecario = idBibliotecario;
+            this.IdUsuario = idUsuario;            this.FechaPrestamo = fechaPrestamo;
             this.FechaDevolucion = fechaDevolucion;
             this.FechaRealDevolucion = fechaRealDevolucion;
             this.EstadoPrestamo = estadoPrestamo;
